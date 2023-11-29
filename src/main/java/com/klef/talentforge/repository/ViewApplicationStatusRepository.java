def0cb3bdef318d0ba5f3825a606b304f0df8800
Mahikiran-Boga <2100030970@kluniversity.in>
2023-11-29 17:04:26 +0530

    Application Status Update

--- src/main/java/com/klef/talentforge/repository/ViewApplicationStatusRepository.java
+++ src/main/java/com/klef/talentforge/repository/ViewApplicationStatusRepository.java
@@ -18,8 +18,8 @@ public interface ViewApplicationStatusRepository extends JpaRepository<ViewAppli
 //	@Query("Select e from ViewApplicationStatus e where e.id=?1 and e.applicationstatustittle=?2")
 //	public List<ViewApplicationStatus> getStatusByIDAndTitle(int id,String Applicationstatustittle);
 	
-	@Query(" from ViewApplicationStatus where id=?2 and applicationstatustittle=?1")
-	public List<ViewApplicationStatus> getStatusByIDAndTitle(String jobtitle,int id);
-	    
-	    
+	@Query("FROM ViewApplicationStatus WHERE id = ?1 AND TRIM(applicationstatustittle) = ?2")
+	public List<ViewApplicationStatus> getStatusByIDAndTitle(int id, String jobtitle);
+	
+
 }
